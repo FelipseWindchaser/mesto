abc7d1bcfcd22d064cbb9b0f3b5ece5fb2caea37
Felipse <samborskaya.r@yandex.ru>
2022-01-28 17:04:32 +0300

    Project_5

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 **Интро**
 
-Яндекс Практикум, Проектная работа № 4.
-Отработка верстки форм, основ Джаваскрипт.
+Яндекс Практикум, Проектная работа № 5.
+Отработка верстки форм, основ Джаваскрипт (редактирование форм, добавление и удаление карточек, лайков).
 
 - [Ссылка на проект в gitPages](https://felipsewindchaser.github.io/mesto/)
 
 **Figma**
 
-- [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+- [Ссылка на макет](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,120 @@
-// variables
-let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__close-button');
-let overlay = document.querySelector('.popup__overlay');
-let profileName = document.querySelector('.profile__title');
-let profileInfo = document.querySelector('.profile__subtitle');
-let profileForm = document.querySelector('.popup__container')
-let editProfileName = document.querySelector('.popup__form-field_type_name');
-let editProfileInfo = document.querySelector('.popup__form-field_type_info');
-// functions
-function editName() {
-  editProfileName.value = profileName.textContent; 
-  editProfileInfo.value = profileInfo.textContent;
-};
-function openProfile() {
-  editName();
-  popup.classList.add('popup_active');
-}
+//
+const initialCards = [
+  {
+    name: 'Цветок',
+    link: 'https://images.unsplash.com/photo-1641842909513-0c59448a17d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+  },
+  {
+    name: 'Папоротник',
+    link: 'https://images.unsplash.com/photo-1641328406522-f94f8a050fd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+  },
+  {
+    name: 'Цветы',
+    link: 'https://images.unsplash.com/photo-1640282385915-c515d654338d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+  },
+  {
+    name: 'Заледеневшие листья',
+    link: 'https://images.unsplash.com/photo-1640177469193-148eeed13e0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+  },
+  {
+    name: 'Горы',
+    link: 'https://images.unsplash.com/photo-1638969710700-f803636a3e85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+  },
+  {
+    name: 'Листья',
+    link: 'https://images.unsplash.com/photo-1639403277293-14a53e4e11ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80'
+  }
+];
 
-function closeProfile() {
-  popup.classList.remove('popup_active');
+const editButton = document.querySelector('.profile__edit-button');
+const popupEditProfile = document.querySelector('.popup_edit-profile');
+const closeButton = document.querySelector('.popup__close-button');
+const overlay = document.querySelector('.popup__overlay');
+const title = document.querySelector('.profile__title');
+const info = document.querySelector('.profile__subtitle');
+const profileForm = document.querySelector('.popup__container');
+const editTitle = popupEditProfile.querySelector('.popup__form-field_type_name');
+const editInfo = popupEditProfile.querySelector('.popup__form-field_type_info');
+const cardTemplate = document.querySelector('.card-template').content;
+const cardsContainer = document.querySelector('.photos');
+const popupAddImage = document.querySelector('.popup_add-image');
+const addImgBtn = document.querySelector('.profile__add-button');
+const closeImgBtn = popupAddImage.querySelector('.popup__close-button');
+const addImgName = popupAddImage.querySelector('.popup__form-field_type_name');
+const addImgUrl = popupAddImage.querySelector('.popup__form-field_type_info');
+const imgName = document.querySelector('.photos__caption');
+const addImgForm = popupAddImage.querySelector('.popup__container');
+const popupImg = document.querySelector('.popup-picture');
+const popupImgCloseBtn = popupImg.querySelector('.popup__close-button');
+const popupImgOverlay = popupImg.querySelector('.popup__overlay_picture');
+
+
+function addCard(text, url) {
+  const card = cardTemplate.cloneNode(true);
+  const cardPicture = card.querySelector('.photos__item');
+  const likeBtn = card.querySelector('.photos__like-button');
+  const delBtn = card.querySelector('.photos__delete-button');
+  cardPicture.src = url;
+  cardPicture.alt = "Картинка " + text;
+  card.querySelector('.photos__caption').textContent = text;
+  likeBtn.addEventListener('click', () => likeBtn.classList.toggle('photos__like-button_active'));
+  cardPicture.addEventListener('click', () => viewPopupImg(text, url));
+  delBtn.addEventListener('click', (evt) => {
+    evt.target.parentElement.remove();
+  });
+  cardsContainer.prepend(card);
+};
+function viewPopupImg(text, url) {
+  const popupCaption = popupImg.querySelector('.popup-picture__caption');
+  const popupImage = popupImg.querySelector('.popup-picture__item');
+  popupCaption.textContent = text;
+  popupImage.src = url;
+  togglePopup(popupImg);
+  
+};
+
+function loadCards(cardList) {
+  cardList.forEach(item => {
+    addCard(item.name, item.link);
+  });
+};
+
+loadCards(initialCards);
+
+function openProfile() {
+  editTitle.value = title.textContent; 
+  editInfo.value = info.textContent;
+  togglePopup(popupEditProfile);
 };
 
 function saveProfileChanges(evt) {
   evt.preventDefault();
-  profileName.textContent = editProfileName.value;
-  profileInfo.textContent = editProfileInfo.value;
-  closeProfile();
+  title.textContent = editTitle.value;
+  info.textContent = editInfo.value;
+  togglePopup(popupEditProfile);
 };
-// listeners
+
+ function togglePopup(popup) {
+   popup.classList.toggle('popup_active');
+ };
+
+ function saveImage(evt) {
+  evt.preventDefault();
+  addCard(addImgName.value, addImgUrl.value);
+  togglePopup(popupAddImage);
+  addImgName.value = '';
+  addImgUrl.value = '';
+  };
+
+
+
+
+addImgBtn.addEventListener('click', () => togglePopup(popupAddImage));
+closeImgBtn.addEventListener('click', () => togglePopup(popupAddImage));
 editButton.addEventListener('click', openProfile);
-closeButton.addEventListener('click', closeProfile);
-overlay.addEventListener('click', closeProfile);
+closeButton.addEventListener('click', () => togglePopup(popupEditProfile));
+overlay.addEventListener('click', () => togglePopup(popupEditProfile));
 profileForm.addEventListener('submit', saveProfileChanges);
-
-
-
+addImgForm.addEventListener('submit', saveImage);
+popupImgCloseBtn.addEventListener('click', () => togglePopup(popupImg));
+popupImgOverlay.addEventListener('click', () => togglePopup(popupImg));
